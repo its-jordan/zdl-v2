0492c9d9fed5d338d3cdf81e530cd83928652edd
its-jordan <s.schultzjordan.jordan@gmail.com>
2024-10-13 22:20:57 -0400

    pushed fix for season 3 trades

--- app/season-3/trades/page.tsx
+++ app/season-3/trades/page.tsx
@@ -30,86 +30,94 @@ interface TradesData {
 // Type assertion for the imported JSON data
 const typedTradesData: TradesData = tradesData;
 
+// export default function PokemonTrades() {
+//   const [selectedPlayer, setSelectedPlayer] = useState<keyof TradesData>(
+//     Object.keys(typedTradesData)[0]
+//   );
+
+//   const handlePlayerChange = (value: string) => {
+//     setSelectedPlayer(value as keyof TradesData);
+//   };
+
+//   const team = teamArray;
+
+//   return (
+//     <main className='mt-20'>
+//       <div className='team-header'>
+//         <div className='team-container trades'>
+//           <h1 className='team-header-name'>Pokémon Trades</h1>
+//           <div className='team-header-trades-player'>
+//             {'> '}
+//             {selectedPlayer}
+//           </div>
+//         </div>
+//         <div className='flex gap-4'>
+//           {/* @ts-expect-error */}
+//           <Select onValueChange={handlePlayerChange} value={selectedPlayer}>
+//             <SelectTrigger className='w-max mb-4'>
+//               <SelectValue placeholder='Select a player' />
+//             </SelectTrigger>
+//             <SelectContent>
+//               {Object.keys(typedTradesData).map((player) => (
+//                 <SelectItem key={player} value={player}>
+//                   <div className='team-select'>
+//                     <img
+//                       className='card-picture'
+//                       src={
+//                         // @ts-expect-error
+//                         team[player].picture
+//                       }
+//                       alt=''
+//                       width={30}
+//                       height={30}
+//                     />
+//                     <div className='team-select-names'>
+//                       <div>
+//                         {
+//                           // @ts-expect-error
+//                           team[player].discord
+//                         }
+//                       </div>
+//                       <div className='team-select-player pr-4'>
+//                         {
+//                           // @ts-expect-error
+//                           team[player].team
+//                         }
+//                       </div>
+//                     </div>
+//                   </div>
+//                 </SelectItem>
+//               ))}
+//             </SelectContent>
+//           </Select>
+//         </div>
+//       </div>
+//       {selectedPlayer && (
+//         <div className='team-trades-container'>
+//           <div className='team-trades'>
+//             {typedTradesData[selectedPlayer].removed.map((pokemon, index) => (
+//               <div key={index} className='text-red-500 pokemon-trade'>
+//                 <LuMinusCircle /> {nameSplit(pokemon)}
+//               </div>
+//             ))}
+//           </div>
+//           <div className='team-trades'>
+//             {typedTradesData[selectedPlayer].added.map((pokemon, index) => (
+//               <div key={index} className='text-green-500 pokemon-trade'>
+//                 <LuPlusCircle /> {nameSplit(pokemon)}
+//               </div>
+//             ))}
+//           </div>
+//         </div>
+//       )}
+//     </main>
+//   );
+// }
+
 export default function PokemonTrades() {
-  const [selectedPlayer, setSelectedPlayer] = useState<keyof TradesData>(
-    Object.keys(typedTradesData)[0]
-  );
-
-  const handlePlayerChange = (value: string) => {
-    setSelectedPlayer(value as keyof TradesData);
-  };
-
-  const team = teamArray;
-
   return (
     <main className='mt-20'>
-      <div className='team-header'>
-        <div className='team-container trades'>
-          <h1 className='team-header-name'>Pokémon Trades</h1>
-          <div className='team-header-trades-player'>
-            {'> '}
-            {selectedPlayer}
-          </div>
-        </div>
-        <div className='flex gap-4'>
-          {/* @ts-expect-error */}
-          <Select onValueChange={handlePlayerChange} value={selectedPlayer}>
-            <SelectTrigger className='w-max mb-4'>
-              <SelectValue placeholder='Select a player' />
-            </SelectTrigger>
-            <SelectContent>
-              {Object.keys(typedTradesData).map((player) => (
-                <SelectItem key={player} value={player}>
-                  <div className='team-select'>
-                    <img
-                      className='card-picture'
-                      src={
-                        // @ts-expect-error
-                        team[player].picture
-                      }
-                      alt=''
-                      width={30}
-                      height={30}
-                    />
-                    <div className='team-select-names'>
-                      <div>
-                        {
-                          // @ts-expect-error
-                          team[player].discord
-                        }
-                      </div>
-                      <div className='team-select-player pr-4'>
-                        {
-                          // @ts-expect-error
-                          team[player].team
-                        }
-                      </div>
-                    </div>
-                  </div>
-                </SelectItem>
-              ))}
-            </SelectContent>
-          </Select>
-        </div>
-      </div>
-      {selectedPlayer && (
-        <div className='team-trades-container'>
-          <div className='team-trades'>
-            {typedTradesData[selectedPlayer].removed.map((pokemon, index) => (
-              <div key={index} className='text-red-500 pokemon-trade'>
-                <LuMinusCircle /> {nameSplit(pokemon)}
-              </div>
-            ))}
-          </div>
-          <div className='team-trades'>
-            {typedTradesData[selectedPlayer].added.map((pokemon, index) => (
-              <div key={index} className='text-green-500 pokemon-trade'>
-                <LuPlusCircle /> {nameSplit(pokemon)}
-              </div>
-            ))}
-          </div>
-        </div>
-      )}
+      <div>Not Updated</div>
     </main>
   );
 }
